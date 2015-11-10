b8a5b1d68d6421848c7c38483e180e7f859d05bd
roo <devnull@localhost>
2015-11-10 10:42:35 +0000

    auto-commit from ed@roo
    Former-commit-id: e52453d77e6e771fdcffa5a75027dc3a17a6e73d

--- check_consistency.py
+++ check_consistency.py
@@ -9,8 +9,11 @@ import webbrowser
 
 from lib.shared import sorted_vus
 from lib.textual_flow import textual_flow
+from populate_db import parse_input_file, populate
 import populate_db
 from lib.shared import UNCL
+from lib.mpisupport import MpiParent, mpi_child, is_parent
+from lib.genealogical_coherence import CyclicDependency
 
 
 def has_unclear(vu, cursor):
@@ -19,45 +22,106 @@ def has_unclear(vu, cursor):
     """
     sql = """SELECT parent FROM reading
              WHERE variant_unit = \"{}\"
-             AND label = \"{}\"""".format(vu, UNCL)
+             AND parent = \"{}\"""".format(vu, UNCL)
     cursor.execute(sql)
     return bool(cursor.fetchall())
 
 
-def main(inputfile, connectivity):
-    working_dir = tempfile.mkdtemp()
-    shutil.copyfile(inputfile, os.path.join(working_dir, inputfile))
-    os.chdir(working_dir)
+class CheckConsistency(MpiParent):
+    def __init__(self, inputfile, connectivity):
+        super().__init__()
+        self.results = {}
+        self.connectivity = connectivity
 
-    html = "<html>\n"
+        self.working_dir = tempfile.mkdtemp()
+        shutil.copyfile(inputfile, os.path.join(self.working_dir, inputfile))
+        os.chdir(self.working_dir)
 
-    with tempfile.NamedTemporaryFile() as db:
-        populate_db.main(inputfile, db.name, force=True)
-        conn = sqlite3.connect(db.name)
-        cursor = conn.cursor()
-        vus = list(sorted_vus(cursor))
-        for vu in vus:
-            if has_unclear(vu, cursor):
-                html += ('<h2>{}</h2><p>UNCL relationships detected</p><hr/>\n'
-                         .format(vu))
-                continue
+        struct, all_mss = parse_input_file(inputfile)
 
-            svg = textual_flow(db.name, vu, connectivity, perfect_only=False)
-            if svg is not None and 'svg' in svg:
-                html += ('<h2>{}</h2><img width="500px" src="{}" alt="{}"/><br/><hr/>\n'
-                         .format(vu, svg, svg))
+        # Make our own temporary db
+        with tempfile.NamedTemporaryFile() as db:
+            populate_db.main(inputfile, db.name, force=True)
+            conn = sqlite3.connect(db.name)
+            cursor = conn.cursor()
+            self.vus = list(sorted_vus(cursor))
+
+            for vu in self.vus:
+                if has_unclear(vu, cursor):
+                    self.results[vu] = "UNCL relationships detected"
+                    continue
+
+                self.mpi_queue.put((struct, all_mss, vu, connectivity))
+
+        self.mpi_wait()
+        self.collate_results()
+
+    def collate_results(self):
+        """
+        Write the HTML file
+        """
+        html = """<html><h1>Consistency: Textual flow diagrams</h1>
+                    <p> for all variant units with complete local stemmata (I.e. no UNCL relationships).</p>
+                    <h1> Connectivity: {}</h1>
+               """.format(self.connectivity)
+        for vu in self.vus:
+            # collate results
+            res = self.results[vu]
+            if res is None:
+                html += '<h2>{}</h2><p>ERROR</p><hr/>\n'.format(vu)
+            elif 'svg' in res:
+                html += ('<h2>{}</h2><img width="500px" src="{}"/><br/><hr/>\n'
+                         .format(vu, res))
             else:
-                html += ('<h2>{}</h2><p>ERROR: {}</p><hr/>\n'
-                         .format(vu, svg))
+                html += '<h2>{}</h2><p>ERROR: {}</p><hr/>\n'.format(vu, res)
 
-    html += "</html>"
-    with open('index.html', 'w') as f:
-        f.write(html)
+        html += "</html>"
+        with open('index.html', 'w') as f:
+            f.write(html)
 
-    print("View the files in {}".format(working_dir))
+        print("View the files in {}".format(self.working_dir))
 
-    print("Opening browser...")
-    webbrowser.open(os.path.join(os.path.join(working_dir, 'index.html')))
+        print("Opening browser...")
+        webbrowser.open(os.path.join(os.path.join(self.working_dir, 'index.html')))
+
+    def mpi_handle_result(self, args, ret):
+        """
+        Handle an MPI result
+        @param args: original args sent to the child
+        @param ret: response from the child
+        """
+        vu = args[2]
+        if ret is None:
+            # error calculating this one
+            self.results[vu] = None
+        else:
+            # recreate the svg locally
+            with open(ret['svgname'], 'wb') as f:
+                f.write(ret['svgdata'])
+
+            self.results[vu] = ret['svgname']
+
+
+def child(struct, all_mss, vu, connectivity):
+    """
+    MPI child function
+    """
+    # Do this in our own temp dir
+    mytemp = tempfile.mkdtemp()
+    os.chdir(mytemp)
+
+    # 1. Populate the db
+    db = '_temp.db'
+    populate(struct, all_mss, db)
+
+    # 2. Make the textual flow diagram
+    try:
+        svg = textual_flow(db, vu, connectivity, perfect_only=False)
+    except CyclicDependency:
+        return None
+
+    with open(svg, 'rb') as f:
+        return {'svgname': svg, 'svgdata': f.read()}
 
 
 if __name__ == "__main__":
@@ -69,5 +133,11 @@ if __name__ == "__main__":
                         help='Maximum allowed connectivity in a textual flow diagram')
 
     args = parser.parse_args()
+    is_parent = is_parent()
 
-    main(args.inputfile, args.connectivity)
+    if is_parent:
+        # parent or only process
+        CheckConsistency(args.inputfile, args.connectivity)
+    else:
+        # child
+        mpi_child(child)
