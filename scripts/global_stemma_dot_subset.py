9a3166a78744542d945faaf94898a0d102f372c6
Andrew Edmondson <ed@rameus.org.uk>
2017-12-30 17:02:31 +0000

    Highlight the included witnesses

--- scripts/global_stemma_dot_subset.py
+++ scripts/global_stemma_dot_subset.py
@@ -3,8 +3,12 @@
 Create a subset of a global stemma, with a list of witnesses and their immediate relations.
 """
 
+COLOUR = '''[color="#b43f3f",
+             fillcolor="#FF8A8A",
+             style=filled]'''
 
-def subset(glob_stem, output, witnesses):
+
+def subset(glob_stem, output, witnesses, highlight):
     """
     Make a new dotfile
     """
@@ -16,6 +20,9 @@ def subset(glob_stem, output, witnesses):
             # Get a list where all witnesses will be entries in their own right
             bits = set(line.replace('"', '').replace(';', '').split())
             if wits_s & bits:
+                if len(bits) == 1:
+                    # node line
+                    line = '{} {};\n'.format(line.rstrip()[:-1], COLOUR)
                 lines.append(line)
 
     lines.append('}')
@@ -32,6 +39,7 @@ if __name__ == "__main__":
     parser.add_argument('global_stemma', help='DOT file representing the global stemma')
     parser.add_argument('output_file', help='Output filename for the subset DOT file')
     parser.add_argument('include_witness', help='Witness name', nargs='+')
+    parser.add_argument('--highlight', help='Highlight the included witnesses', action='store_true')
     args = parser.parse_args()
 
-    subset(args.global_stemma, args.output_file, args.include_witness)
+    subset(args.global_stemma, args.output_file, args.include_witness, args.highlight)
