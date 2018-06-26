cf85125bdd3905ba8270b197a896d101ba4c6271
Ed <edmondac@users.noreply.github.com>
2018-06-26 22:33:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='CBGM',
-    version='2.1',
+    version='2.2',
     packages=['CBGM'],
     url='https://github.com/edmondac/CBGM',
     license='MIT',
